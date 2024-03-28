a3e686f20ff2f9dcb9d90e2c9a76932dc65117ae
Lucas Ferreira Garcez <137219750+lucasfgarcez@users.noreply.github.com>
2024-03-27 21:32:14 -0300

    Update do programa

--- Projeto01/PrevisaoConsumoEletrico.R
+++ Projeto01/PrevisaoConsumoEletrico.R
@@ -1,3 +1,5 @@
+# INTRODUÇÃO ####
+
 # Curso: Big Data Analytics com R e Microsoft Azure
 # Machine Learning
 # Data Science Academy
@@ -7,8 +9,11 @@
 # de Energia de Carros Elétricos
 
 # Muda o diretório de trabalho
-setwd('/Projeto01')
+setwd('C:/FCD/BigDataRAzure/Cap20/Projeto01')
 getwd()
+
+# Caso seja necessário instalar um pacote
+#install.packages('pacote')
 
 # Carrega os pacotes
 library(readxl)
@@ -18,6 +23,13 @@ library(RColorBrewer)
 library(ggplot2)
 library(gridExtra)
 library(randomForest)
+library(knitr)
+library(car)
+library(MASS)
+library(neuralnet)
+library(fastDummies)
+
+# PREPARANDO OS DADOS ####
 
 # Carrega dataframe
 df <- read_excel('dataset/FEV-data-Excel.xlsx')
@@ -41,44 +53,41 @@ colSums(is.na(df))
 df1 <- na.omit(df)
 sum(is.na(df1))
 dim(df1)
-(nrow(df) - nrow(df1))/nrow(df) # Percentual de linhas removidas em relação ao dataset original.
+(nrow(df) - nrow(df1))/nrow(df) # Percentual de linhas removidas.
 
 # Ao total, 9 observações com valores NA foram removidas,
 # representando 20,7% do dataframe original.
 
-# Nomes das colunas
+# Extraindo nomes das colunas
 colunas <- colnames(df1)
 colunas
 
 # Renomeando colunas
-colunas[1] <- 'Car_Name'
-colunas[4] <- 'Min_Price'
-colunas[5] <- 'Engine_Power'
-colunas[6] <- 'Max_Torque'
-colunas[7] <- 'Brakes_Type'
-colunas[8] <- 'Drive_Type'
-colunas[9] <- 'Batt_Cap'
+colunas[1] <- 'CarName'
+colunas[4] <- 'MinPrice'
+colunas[5] <- 'EnginePower'
+colunas[6] <- 'MaxTorque'
+colunas[7] <- 'BrakesType'
+colunas[8] <- 'DriveType'
+colunas[9] <- 'BattCap'
 colunas[10] <- 'Range'
 colunas[11] <- 'Wheelbase'
 colunas[12] <- 'Length'
 colunas[13] <- 'Width'
 colunas[14] <- 'Height'
-colunas[15] <- 'Min_Empty_Weight'
-colunas[16] <- 'Perm_Gross_Weight'
-colunas[17] <- 'Max_Load_Cap'
+colunas[15] <- 'MinEmptyWeight'
+colunas[16] <- 'PermGrossWeight'
+colunas[17] <- 'MaxLoadCap'
 colunas[18] <- 'Seats'
 colunas[19] <- 'Doors'
-colunas[20] <- 'Tire_Size'
-colunas[21] <- 'Max_Speed'
-colunas[22] <- 'Boot_Cap'
-colunas[23] <- 'Acc_0_100'
-colunas[24] <- 'Max_DC_Charg'
-colunas[25] <- 'Energy_Consumption'
+colunas[20] <- 'TireSize'
+colunas[21] <- 'MaxSpeed'
+colunas[22] <- 'BootCap'
+colunas[23] <- 'Acc100'
+colunas[24] <- 'MaxDCCharg'
+colunas[25] <- 'EnergyConsumption'
 colnames(df1) <- colunas
 remove(colunas)
-
-# Visualiza os dados
-View(df1)
 
 # Convertendo as variáveis do tipo caracter para fator.
 df1 <- df1 %>% 
@@ -99,122 +108,148 @@ View(df1)
 # Resumo estatístico
 summary(df1)
 
-# A variável alvo é a 'Energy_Consumption'
+# A variável alvo é a 'EnergyConsumption'
 png('plots/plot1.png', width = 600, height = 480) # Salvando plot em um PNG
-plot(x=c(1:42), y=df1$Energy_Consumption, 
+plot(x=c(1:42), y=df1$EnergyConsumption, 
      main = 'Consumo de Energia Médio em kWH/100km',
      xlab = 'Nº do Carro (42 ao total)',
      ylab = 'Consumo de Energia (kWH/100km)')
 dev.off()
 
-# ANÁLISE EXPLORATÓRIA #
+# ANÁLISE EXPLORATÓRIA ####
+
+# ANÁLISE UNIVARIADA ####
+
+# Histograma do consumo de energia
+Hist_Consumo <- ggplot(df1, aes(x=EnergyConsumption, fill=DriveType)) +
+  geom_histogram(binwidth = 2.5) +
+  labs(title = 'Consumo de Energia por Marca', 
+       y = 'Consumo de Energia (kWH/100km)',
+       x = 'Marca',
+       fill = 'Tipo de Tração')
+png('plots/plot2.png', width = 600, height = 480)
+Hist_Consumo
+dev.off()
 
 
-# Percebe-se que algumas marcas (Audi e Porsche) consomem mais energia
-# ao mesmo tempo que percebe-se que seus tipos de tração são de 4 rodas.
-Consumo_Marcas <- ggplot(df1, aes(x=Make, y=Energy_Consumption, fill=Drive_Type)) +
+# Boxplot de consumo de energia por marca e por tipo de tração
+Consumo_Marcas <- ggplot(df1, aes(x=Make, y=EnergyConsumption, fill=DriveType)) +
   geom_boxplot() +
   labs(title = 'Consumo de Energia por Marca', 
        y = 'Consumo de Energia (kWH/100km)',
        x = 'Marca',
        fill = 'Tipo de Tração') +
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
-png('plots/plot2.png', width = 600, height = 480) # Salvando plot em um PNG
+png('plots/plot3.png', width = 600, height = 480) # Salvando plot em um PNG
 Consumo_Marcas
 dev.off()
 
-# Neste plot percebe-se que cada tipo de tração possui consumos de energia
-# semelhantes. Uma marca (Citroën) é um outlier.
-Consumo_Marcas_por_Tracao <- ggplot(df1, aes(x=Make, y=Energy_Consumption, 
-                                             fill=Drive_Type)) +
-  geom_boxplot() +
+# Percebe-se que algumas marcas (Audi e Porsche) consomem mais energia
+# ao mesmo tempo que percebe-se que seus tipos de tração são de 4 rodas.
+
+
+# Boxplot de consumo de energia por marca dividido em janelas de tração
+Consumo_Marcas_por_Tracao <- ggplot(df1, aes(x=Make, y=EnergyConsumption, 
+                                             fill=DriveType)) +
+  geom_boxplot()  + 
+  coord_cartesian(ylim = c(13, 30)) +
   labs(title = 'Consumo de Energia por Marca', 
        y = 'Consumo de Energia (kWH/100km)',
        x = 'Marca',
        fill = 'Tipo de Tração') +
   theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
-  facet_wrap(~Drive_Type, scale="free")
-png('plots/plot3.png', width = 600, height = 480) # Salvando plot em um PNG
+  facet_wrap(~DriveType, scale="free")
+png('plots/plot4.png', width = 600, height = 480) # Salvando plot em um PNG
 Consumo_Marcas_por_Tracao
 dev.off()
 
-# Como visto acima, os carros com tração nas 4 rodas consomem mais
-# energia comparado aos de tração em 2 rodas. O plot abaixo resume
-# o plot anterior.
-Consumo_Tracao <- ggplot(df1, aes(x=Drive_Type, y=Energy_Consumption, 
-                                  fill=Drive_Type)) +
+# Neste plot percebe-se que cada tipo de tração possui consumos de energia
+# semelhantes. A marca Citroën é um outlier, pois consome muito além da média
+# dos carros com tração dianteira.
+
+# Boxplot de consumo de energia por freio
+Consumo_Tracao <- ggplot(df1, aes(x=DriveType, y=EnergyConsumption, 
+                                  fill=DriveType)) +
   geom_boxplot() +
   labs(title = 'Consumo de Energia por Tração', 
        y = 'Consumo de Energia (kWH/100km)',
        x = 'Tipo de Tração',
        fill = 'Tipo de Tração')
-png('plots/plot4.png', width = 600, height = 480) # Salvando plot em um PNG
+png('plots/plot5.png', width = 600, height = 480) # Salvando plot em um PNG
 Consumo_Tracao
 dev.off()
+
+# Como visto acima, os carros com tração nas 4 rodas consomem mais
+# energia comparado aos de tração em 2 rodas. O plot abaixo resume
+# o plot anterior.
 
 # Boxplot do consumo de energia de acordo com tipo de freio (a disco
 # frontal + traseiro e a disco frontal + tambor traseiro) e os tipos
 # de tração.
-Consumo_Freio <- ggplot(df1, aes(x=Brakes_Type, y=Energy_Consumption,
-                                 fill=Drive_Type)) +
+Consumo_Freio <- ggplot(df1, aes(x=BrakesType, y=EnergyConsumption,
+                                 fill=DriveType)) +
   geom_boxplot() +
   labs(title = 'Consumo de Energia por Freio', 
        y = 'Consumo de Energia (kWH/100km)',
        x = 'Tipo de Freio',
        fill = 'Tração')
-png('plots/plot5.png', width = 600, height = 480) # Salvando plot em um PNG
+png('plots/plot6.png', width = 600, height = 480) # Salvando plot em um PNG
 Consumo_Freio
 dev.off()
 
 # Boxplot do consumo de energia de acordo com número de assentos.
-Consumo_Assentos <- ggplot(df1, aes(x=Seats, y=Energy_Consumption, fill=Seats)) +
+Consumo_Assentos <- ggplot(df1, aes(x=Seats, y=EnergyConsumption, fill=Seats)) +
   geom_boxplot() +
   labs(title = 'Consumo de Energia por Assentos', 
        y = 'Consumo de Energia (kWH/100km)',
        x = 'Numero de Assentos',
        fill = 'Nº Assentos')
-png('plots/plot6.png', width = 600, height = 480) # Salvando plot em um PNG
+png('plots/plot7.png', width = 600, height = 480) # Salvando plot em um PNG
 Consumo_Assentos
 dev.off()
 
 # Boxplot do consumo de energia de acordo com número de portas.
-Consumo_Portas <- ggplot(df1, aes(x=Doors, y=Energy_Consumption, fill=Doors)) +
+Consumo_Portas <- ggplot(df1, aes(x=Doors, y=EnergyConsumption, fill=Doors)) +
   geom_boxplot() +
   labs(title = 'Consumo de Energia por Portas', 
        y = 'Consumo de Energia (kWh/100km)',
-       x = 'Numero de Assentos',
+       x = 'Numero de Portas',
        fill = 'Nº Portas')
-png('plots/plot7.png', width = 600, height = 480) # Salvando plot em um PNG
+png('plots/plot8.png', width = 600, height = 480) # Salvando plot em um PNG
 Consumo_Portas
 dev.off()
 
 # Agrupando dados do tipo fator para gerar gráficos do tipo pizza
 # mostrando o número total de cada tipo.
 Brakes_Type_Total <- df1 %>%
-  group_by(Brakes_Type) %>%
+  group_by(BrakesType) %>%
   summarise(total_type=n())
 
-plot1 <- ggplot(Brakes_Type_Total, aes(x="", y=total_type, fill=Brakes_Type))+
+plot1 <- ggplot(Brakes_Type_Total, aes(x="", y=total_type, fill=BrakesType))+
   geom_bar(width = 1, stat = "identity") +
-  coord_polar("y", start=0) +
+  coord_polar("y") +
   theme_minimal() +
+  geom_text(aes(label=total_type), position = position_stack(vjust = 0.5)) +
   labs(title = 'Distribuição de Acordo com Freio', 
        y = NULL,
        x = NULL,
-       fill = 'Tipo de Freio')
+       fill = 'Tipo de Freio') +
+  theme_void()
 
 Drive_Type_Total <- df1 %>%
-  group_by(Drive_Type) %>%
+  group_by(DriveType) %>%
   summarise(total_type=n())
 
-plot2 <- ggplot(Drive_Type_Total, aes(x="", y=total_type, fill=Drive_Type))+
+plot2 <- ggplot(Drive_Type_Total, aes(x="", y=total_type, fill=DriveType))+
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start=0) +
   theme_minimal() +
+  geom_text(aes(label=total_type), position = position_stack(vjust = 0.5)) +
   labs(title = 'Distribuição de Acordo com Tração', 
        y = NULL,
        x = NULL,
-       fill = 'Tipo de Tração')
+       fill = 'Tipo de Tração') +
+  theme_void()
 
 Seats_Total <- df1 %>%
   group_by(Seats) %>%
@@ -224,10 +259,12 @@ plot3 <- ggplot(Seats_Total, aes(x="", y=total_type, fill=Seats))+
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start=0) +
   theme_minimal() +
+  geom_text(aes(label=total_type), position = position_stack(vjust = 0.5)) +
   labs(title = 'Distribuição de Acordo com Assentos', 
        y = NULL,
        x = NULL,
-       fill = 'Nº de Assentos')
+       fill = 'Nº de Assentos') +
+  theme_void()
 
 Doors_Total <- df1 %>%
   group_by(Doors) %>%
@@ -237,98 +274,171 @@ plot4 <- ggplot(Doors_Total, aes(x="", y=total_type, fill=Doors))+
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start=0) +
   theme_minimal() +
+  geom_text(aes(label=total_type), position = position_stack(vjust = 0.5)) +
   labs(title = 'Distribuição de Acordo com Portas', 
        y = NULL,
        x = NULL,
-       fill = 'Nº de Portas')
+       fill = 'Nº de Portas') +
+  theme_void()
 
 # Gráficos de pizza agrupados
-png('plots/plot8.png', width = 600, height = 480) # Salvando plot em um PNG
+png('plots/plot9.png', width = 600, height = 480) # Salvando plot em um PNG
 grid.arrange(plot1, plot2, plot3, plot4, ncol=2)
 dev.off()
 
-# ANÁLISE DE REGRESSÃO LINEAR #
+# ANÁLISE MULTIVARIADA ####
 
-# O método utilizado será de regressão linear por se tratar de uma variável
-# numérica e não categórica.
+# Gerando um Plot de Correlação entre as Variáveis Numéricas
+nums <- unlist(lapply(df1, is.numeric), use.names = FALSE)
+corr <- cor(df1[,nums])
+png('plots/plot10.png', width = 600, height = 480)
+corrplot(corr, type="lower", tl.col="black", order='FPC',
+         diag=FALSE)
+dev.off()
+
+# Correlação com a variável alvo
+df2 <- df1[, nums]
+View(df2)
+sort(cor(df2[-18], df2$EnergyConsumption), decreasing = TRUE)
+
+# ANÁLISE DE MODELOS PREDITIVOS ####
+
+# Modelo 01
+
+# O método utilizado será de regressão linear.
+formula1 <- EnergyConsumption ~ . -CarName-Make-Model
+modelo_v1 <- lm(formula1, data = df1)
+summary(modelo_v1)
+
+# O valor de R^2 é alto, de 98%, o que é bom, porém o modelo leva em conta
+# muitas variáveis, tornando-o complexo.
+
+# Detectando Colinearidade
+kable(vif(modelo_v1), align = 'c')
+
+# Considerando a última coluna da função acima, eliminaremos as variáveis
+# com valores acima de 10, que são consideradas variâncias altas.
+
+# Modelo 02 
+
+# Removendo algumas variáveis com variâncias altas encontradas
+# na função VIF acima.
+formula2 <- EnergyConsumption ~ MinPrice+MaxTorque+BrakesType+DriveType+Range+Width+Height+MaxLoadCap+Seats+Doors+TireSize+BootCap+Acc100+MaxDCCharg
+modelo_v2 <- lm(formula2, data = df1)
+summary(modelo_v2)
+
+# O modelo ainda apresenta um valor R^2 elevado de 94%, agora com menos 
+# variáveis, o que deixa o modelo menos complexo e mais generalizável.
+
+# Checando colinearidade do modelo 02
+kable(vif(modelo_v2), align = 'c')
+
+# Os valores estão entre 1 e 5, então pode-se deixá-los como variáveis
+# explicativas do modelo.
+
+# Modelo 03
 
 # Verificação das variáveis mais relevantes para o modelo em regressão linear
 # utilizando random forest para análise. Inicialmente foram removidas variáveis
 # que não serão levadas em consideração (nome do carro, marca e modelo).
-formula <- Energy_Consumption ~ . - Car_Name - Make - Model
-
-modelo <- randomForest(formula, 
+varimp <- randomForest(EnergyConsumption ~ MinPrice+MaxTorque+BrakesType+DriveType+Range+Width+Height+MaxLoadCap+Seats+Doors+TireSize+BootCap+Acc100+MaxDCCharg,
                        data = df1, 
                        ntree = 100, 
                        nodesize = 10,
                        importance = TRUE)
+varImpPlot(varimp)
 
-varImpPlot(modelo)
+# Reduzindo algumas variáveis de acordo com o nível de importância delas.
+formula3 <- EnergyConsumption ~ MinPrice+MaxTorque+DriveType+Width+MaxLoadCap+BootCap+Acc100
+modelo_v3 <- lm(formula3, data = df1)
+summary(modelo_v3)
 
-# H0: Não há efeito das variáveis escolhidas em Energy_Consumption.
-# H1: Há efeito das variáveis escolhidas em Energy_Consumption.
+# O valor de R^2 do modelo 03 é de 86%. 
 
-summary(lm(formula, data = df1))
+# Coeficiente AIC
+step <- stepAIC(modelo_v3, direction = "both", trace = FALSE)
+summary(step)
+summary(step)$coeff
+summary(step)$r.squared
 
-# O valor p neste modelo ficou abaixo de 0,05. Rejeita-se a hipótese nula.
+# Pode-se notar que pela AIC, as variáveis que menos perdem informação para
+# explicar a variável alvo são MaxTorque, DriveType, MaxLoadCap e Acc100.
 
-# Após a primeira verificação, foram removidas outras variáveis que tem pouca
-# ou nenhuma influência para o modelo.
-formula2 <- Energy_Consumption ~ . - Car_Name - Make - Model - Doors - 
-  Batt_Cap - Seats - Height - Brakes_Type - Range - Acc_0_100 - 
-  Max_DC_Charg - Max_Speed - Width - Drive_Type - Boot_Cap
+# Modelo 04
 
-modelo2 <- randomForest(formula2, 
-                       data = df1, 
-                       ntree = 100, 
-                       nodesize = 10,
-                       importance = TRUE)
+# Utilizando as variáveis encontradas na AIC.
+formula4 <- EnergyConsumption ~ MaxTorque+DriveType+MaxLoadCap+Acc100
+modelo_v4 <- lm(formula4, data = df1)
+summary(modelo_v4)
 
-varImpPlot(modelo2)
+# Com isto, o modelo consegue explicar cerca de 86% da variável alvo com o
+# mínimo de variáveis explicatórias. Este é um modelo robusto.
 
-summary(lm(formula2, data = df1))
+# Detectando colinearidade
+kable(vif(modelo_v4), align = 'c')
 
-# O valor p neste modelo2 ficou abaixo de 0,05. Rejeita-se a hipótese nula.
-
-# Reduzindo mais um pouco o número de variáveis e fazendo algumas combinações
-# entre elas, encontraram-se as variáveis abaixo que melhor explicam o modelo.
-formula3 <- Energy_Consumption ~ Max_Torque + Length + 
-  Perm_Gross_Weight + Max_Load_Cap + Tire_Size
-
-modelo3 <- randomForest(formula3, 
-                        data = df1, 
-                        ntree = 100, 
-                        nodesize = 10,
-                        importance = TRUE)
-
-varImpPlot(modelo3)
-
-summary(lm(formula3, data = df1))
-
-# O valor p neste modelo3 ficou abaixo de 0,05. Rejeita-se a hipótese nula.
-
-# Gravação do modelo em uma variável e teste de predição do modelo. Resultado
-# gravado em uma nova coluna para comparativo.
-rlmodelo <- lm(formula3, data = df1)
-
-df2 <- df1 # Criado uma duplicata do dataset
-df2$Previsao <- predict(rlmodelo, data = df1)
-
-df2 <- df2[order(df2$Energy_Consumption),] # Ordenação dos dados para melhor visualização.
-
-# Plot comparando a variável alvo Energy_Consumption e a variável de previsão
-# do modelo.
-Comparativo_Modelos <- ggplot(df2) +
-  geom_line(aes(x = c(1:42), y = Energy_Consumption, colour="Consumo_Dataset_Original"), size = 1.25) + 
-  geom_line(aes(x = c(1:42), y = Previsao, colour="Consumo_Previsao"), size = 1.25) +
-  scale_color_manual(name = "Legenda", values = c("Consumo_Dataset_Original" = "darkblue", "Consumo_Previsao" = "red")) +
-  labs(title = 'Comparação entre variável alvo e variável prevista (ordenado de forma crescente)', 
-       y = 'Consumo de Energia (kWH/100km)',
-       x = 'Carros (42 ao total)')
-png('plots/plot8.png', width = 800, height = 480) # Salvando plot em um PNG
-Comparativo_Modelos
+# Plotando gráfico de resíduos versus os valores ajustados
+modelo_v4$fitted.values
+png('plots/plot11.png', width = 600, height = 480)
+qqPlot(modelo_v4, main = "Normal Q-Q plot")
 dev.off()
 
-# Como vimos no gráfico anterior, o modelo de machine learning
-# conseguiu obter resultados aproximados ao dataset inicial. Assim, o modelo
-# obteve grau satisfatório.
+# Modelo 05
+
+# Utilizando Random Forest
+modelo_v5 <- randomForest(formula1,
+                          data=df1,
+                          ntree = 500,
+                          nodesize = 10,
+                          importance = FALSE)
+modelo_v5
+
+# Este modelo com Random Forest consegue explicar até 83% da variância.
+
+# Modelo 06
+
+# Normalizando variáveis
+func_normaliza <- function(x){
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+df3 <- as.data.frame(lapply(df1[,-c(1:4,7:9, 18:20)], FUN = func_normaliza))
+df3_final <- data.frame(df3, df1[,c(7:9, 18:20)])
+View(df3_final)
+
+# Criando variáveis dummy
+df4 <- dummy_cols(df3_final[c(2,10,13,15,17)])
+View(df4)
+colnames(df4)
+colnames(df4)[6] <- "DriveType_2WD_front"
+colnames(df4)[7] <- "DriveType_2WD_rear"
+colnames(df4)
+
+# Utilizando Redes Neurais
+modelo_v6 <- neuralnet(EnergyConsumption ~ MaxTorque+DriveType_2WD_front+DriveType_2WD_rear+DriveType_4WD+MaxLoadCap+Acc100,
+                       data=df4,
+                       hidden=4)
+plot(modelo_v6)
+previsoes <- predict(modelo_v6, df4[,-c(4:5)])
+
+# Desnormalizando variáveis
+func_desnormaliza <- function(n){
+  return (n*(max(df1$EnergyConsumption) - min(df1$EnergyConsumption)) + min(df1$EnergyConsumption))
+}
+
+# Encontrando os valores de previsão final sem normalização e
+# comparando com valores do dataset original.
+previsoes_desn <- as.data.frame(sapply(previsoes, FUN = func_desnormaliza))
+colnames(previsoes_desn) <- 'EnergyConsumption'
+previsoes_desn <- data.frame(previsoes_desn, df1$EnergyConsumption)
+View(previsoes_desn)
+
+# Percebe-se que este modelo com Redes Neurais não perfomou bem ao tentar
+# prever os valores numéricos.
+
+# Para este projeto, o modelo 04 será o escolhido como melhor modelo
+# por sua boa capacidade de previsão, explicando 86% da variância do dataset
+# e por ser simples, com poucas variáveis explicatórias.
+summary(modelo_v4)
+
+# Fim do projeto.
